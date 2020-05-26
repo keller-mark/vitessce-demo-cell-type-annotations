f704f78d1299869393883803720b50d2c14858ea
mkeller <7525285+keller-mark@users.noreply.github.com>
2020-05-26 19:44:21 -0400

    Print all paths up the DAG

--- src/generate_cell_sets.py
+++ src/generate_cell_sets.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import networkx
+from pprint import pprint
 
 from constants import COLUMNS, CL_ROOT_ID
 from utils import init_tree, load_cl_obo_graph
@@ -41,6 +42,46 @@ def generate_hierarchical_cell_sets(df, cl_obo_file):
     # Load the cell ontology DAG
     graph, id_to_name, name_to_id = load_cl_obo_graph(cl_obo_file)
 
+
+    def check_multi_parents(node_id, node_parent_ids):
+        # Warn if the current node has multiple parents.
+        num_parents = len(node_parent_ids)
+        if num_parents > 1:
+            parent_names = "[ " + ", ".join([ f"{p_id} ({id_to_name[p_id]})" for p_id, _, _ in node_parent_ids ]) + " ]"
+            """
+            print((
+                f"WARN: {node_id} ({id_to_name[node_id]}) has "
+                f"{num_parents} parents: {parent_names}."
+            ))
+            """
+    
+    def get_parents(node_id):
+
+        if node_id == CL_ROOT_ID:
+            return [
+                [node_id]
+            ]
+
+        # Get ancestors of the cell type
+        # (counterintuitive that the function is called descendants)
+        ancestor_term_set = networkx.descendants(graph, node_id)
+
+        # Make sure the cell type has an ancestor
+        # with the 'cell' root ID
+        assert(CL_ROOT_ID in ancestor_term_set)
+
+        # Get the parents of the current node.
+        node_parents = list(graph.out_edges(node_id, keys=True))
+
+        up_dag_paths = []
+        for node_parent in node_parents:
+            _, curr_parent_id, relationship = node_parent
+            if relationship == "is_a":
+                parent_paths = get_parents(curr_parent_id)
+                for parent_path in parent_paths:
+                    up_dag_paths.append([node_id] + parent_path)
+        return up_dag_paths
+
     ancestors_and_sets = []
 
     for cell_type, cell_type_df in df.groupby(COLUMNS.ANNOTATION.value):
@@ -64,6 +105,14 @@ def generate_hierarchical_cell_sets(df, cl_obo_file):
         # Initialize the current node ID to the cell type of interest
         curr_node_id = node_id
 
+        paths_up = get_parents(node_id)
+        named_paths_up = [ [id_to_name[n_id] for n_id in path_nodes] for path_nodes in paths_up ]
+        print()
+        print(f"{id_to_name[node_id]} has {len(paths_up)} paths up to {CL_ROOT_ID} ({id_to_name[CL_ROOT_ID]}):")
+        for named_path_nodes in named_paths_up:
+            print(named_path_nodes)
+        print()
+
         # Construct a list of ancestors, inclusive.
         # [node_id, parent_id, grandparent_id, ...]
         ancestors = [curr_node_id]
@@ -71,12 +120,7 @@ def generate_hierarchical_cell_sets(df, cl_obo_file):
         # Get the parents of the current node.
         curr_node_parents = list(graph.out_edges(curr_node_id, keys=True))
 
-        # Warn if the current node has multiple parents.
-        if len(curr_node_parents) > 1:
-            print((
-                f"WARN: {curr_node_id} has "
-                f"{len(curr_node_parents)} parents."
-            ))
+        check_multi_parents(curr_node_id, curr_node_parents)
 
         # Select the first (hopefully only) parent node.
         _, curr_parent_id, relationship = curr_node_parents[0]
@@ -87,11 +131,7 @@ def generate_hierarchical_cell_sets(df, cl_obo_file):
             curr_node_parents = list(graph.out_edges(curr_node_id, keys=True))
 
             # Warn if the current node has multiple parents.
-            if len(curr_node_parents) > 1:
-                print((
-                    f"WARN: {curr_node_id} has "
-                    f"{len(curr_node_parents)} parents."
-                ))
+            check_multi_parents(curr_node_id, curr_node_parents)
 
             # Select the first (hopefully only) parent node.
             _, curr_parent_id, relationship = curr_node_parents[0]
@@ -104,7 +144,7 @@ def generate_hierarchical_cell_sets(df, cl_obo_file):
 
         named_ancestors = [id_to_name[a] for a in ancestors]
         named_ancestors_reversed = list(reversed(named_ancestors))
-        print(named_ancestors_reversed)
+        #print(named_ancestors_reversed)
 
         ancestors_and_sets.append((
             named_ancestors_reversed,
@@ -125,7 +165,7 @@ def generate_hierarchical_cell_sets(df, cl_obo_file):
             for ancestors, cell_set in ancestors_and_sets:
                 ancestors.pop(0)
         else:
-            print(unique_level_cell_types)
+            #print(unique_level_cell_types)
             break
 
     # Construct a hierarchy of cell types.
